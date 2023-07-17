a49819040b0b2ee52353063827268f831fa5efdd
Henk Kin <HenkK@4dotnet.nl>
2023-07-17 13:56:49 +0200

    Added Blog related entities via dynamic registration on ModelBuilder

---
+++ EntityCloner.Microsoft.EntityFrameworkCore.Tests/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityCloner.Microsoft.EntityFrameworkCore.Tests.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void RegisterAllEntities<Entity>(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+    {
+        IEnumerable<Type> types = assemblies.SelectMany(a => a.GetExportedTypes()).Where(c => c.IsClass && !c.IsAbstract && c.IsPublic &&
+            typeof(Entity).IsAssignableFrom(c));
+        foreach (Type type in types)
+            modelBuilder.Entity(type);
+    }
+}
